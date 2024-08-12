ff1bb79ba5825fcb885c0938d72fd65893084a60
Sarvesh singh <sarveshadhikari905@gmail.com>
2024-08-12 12:16:09 +0530

    size of operator

---
+++ size of operator.cpp
@@ -0,0 +1,12 @@
+// Size of operator 
+
+#include <iostream>
+using namespace std;
+main()
+{
+	//cout << sizeof (int) << endl;
+	//cout << sizeof (char);
+	
+	int a =10;
+	cout<< sizeof (a)<< endl;
+}
