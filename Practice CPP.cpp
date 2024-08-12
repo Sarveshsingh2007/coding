f8e995d19fa2d817bf1ec4916b63520b4840a513
Sarvesh singh <sarveshadhikari905@gmail.com>
2024-08-12 11:55:27 +0530

    new

---
+++ Practice CPP.cpp
@@ -0,0 +1,14 @@
+// Precedence of operator 
+// precedence tells us the priority of the operator which will be used first 
+ 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a= 10, b=23 , c= 5 , result;
+	result = a+b+c;
+	cout<< "Result : " << result << endl;
+	return 0;
+}
+
---
+++ increment and decrement.cpp
@@ -0,0 +1,17 @@
+// Increment and decrement operators
+
+// ++a = increment a by 1, then use the new value of a
+// a++ = use value of a, the increment a by 1
+// --b = decrement b by 1, then use the new value of b
+// b-- = use the current value of b, then decrement by 1
+
+#include <iostream>
+using namespace std;
+main()
+{
+	int a =3;
+	cout<< a++ << endl;
+	cout<< a << endl;
+	cout<< ++a << endl;
+	cout<< a << endl;
+}
